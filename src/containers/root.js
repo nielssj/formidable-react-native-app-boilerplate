5157a8f72c1c615b274c5c65ea8c26962c7ff5a1
niels <nm@9la.dk>
2016-03-18 19:56:58 +0100

    Remove /native path from react-native dependency import

--- src/containers/root.js
+++ src/containers/root.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 import React from "react-native";
-import { Provider } from "react-redux/native";
+import { Provider } from "react-redux";
 import configureStore from "../store/configure-store";
 import Scene from "../components/scene";
 
